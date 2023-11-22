a04abdab01c8f91be8d5f200428055123e311e4d
mottakinrahat <rahatchowhdury661@gmail.com>
2023-11-22 23:17:19 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import cors from "cors";
+
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
